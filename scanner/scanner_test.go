fa82aed5cec59b0d1c6e0028427cc9dac34a1ef5
Samuel Jean <jamael.seun@gmail.com>
2015-11-28 19:02:06 -0500

    Added TestRunAndWaitLocalhostTCP6 test

--- scanner/scanner_test.go
+++ scanner/scanner_test.go
@@ -11,22 +11,49 @@ import (
 	"time"
 )
 
+func expectPortsAndClose(ls map[string]net.Listener, ports []Port) error {
+	for p, l := range ls {
+		var ok bool
+		for _, port := range ports {
+			if z := fmt.Sprintf("%d", port.Number); p == z {
+				ok = true
+			}
+		}
+
+		if !ok {
+			return fmt.Errorf("port %s was expected", p)
+		}
+
+		l.Close()
+	}
+
+	return nil
+}
+
+func newTCPListeners(addr string, n int) (map[string]net.Listener, error) {
+	ls := make(map[string]net.Listener, n)
+	for i := 0; i < n; i++ {
+		l, err := net.Listen("tcp", addr+":0")
+		if err != nil {
+			return nil, err
+		}
+		_, port, err := net.SplitHostPort(l.Addr().String())
+		if err != nil {
+			return nil, err
+		}
+		ls[port] = l
+	}
+	return ls, nil
+}
+
 // TestRunAndWaitLocalhostTCP4 listens on 64 available ports
 // on localhost IP version 4 address and attempt a full TCP
 // port scan.  All 64 ports must be discovered by the scanner
 // for the test to succeed.
 func TestRunAndWaitLocalhostTCP4(t *testing.T) {
-	ls := make(map[string]net.Listener, 64)
-	for i := 0; i < 64; i++ {
-		l, err := net.Listen("tcp", "127.0.0.1:0")
-		if err != nil {
-			t.Fatal(err)
-		}
-		_, port, err := net.SplitHostPort(l.Addr().String())
-		if err != nil {
-			t.Fatal(err)
-		}
-		ls[port] = l
+	ls, err := newTCPListeners("127.0.0.1", 64)
+	if err != nil {
+		t.Fatal(err)
 	}
 
 	timeout := time.Duration(time.Second)
@@ -38,19 +65,31 @@ func TestRunAndWaitLocalhostTCP4(t *testing.T) {
 	// 128 workers is enough, do not crank it up.
 	<-RunAndWait("tcp", hosts, timeout, 128)
 
-	ports := hosts[0].Ports
-	for p, l := range ls {
-		var ok bool
-		for _, port := range ports {
-			if z := fmt.Sprintf("%d", port.Number); p == z {
-				ok = true
-			}
-		}
+	if err := expectPortsAndClose(ls, hosts[0].Ports); err != nil {
+		t.Fatal(err)
+	}
+}
 
-		if !ok {
-			t.Fatalf("port %s not found in %v", p, ports)
-		}
+// TestRunAndWaitLocalhostTCP6 listens on 64 available ports
+// on localhost IP version 6 address and attempt a full TCP
+// port scan.  All 64 ports must be discovered by the scanner
+// for the test to succeed.
+func TestRunAndWaitLocalhostTCP6(t *testing.T) {
+	ls, err := newTCPListeners("[::1]", 64)
+	if err != nil {
+		t.Fatal(err)
+	}
 
-		l.Close()
+	timeout := time.Duration(time.Second)
+	hosts := NewHosts("::1")
+	if len(hosts) < 1 {
+		t.Fatal("NewHosts() failed")
+	}
+
+	// 128 workers is enough, do not crank it up.
+	<-RunAndWait("tcp", hosts, timeout, 128)
+
+	if err := expectPortsAndClose(ls, hosts[0].Ports); err != nil {
+		t.Fatal(err)
 	}
 }
